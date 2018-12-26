bc1d68818ee2e04b0360f0b46eed171ae86729b1
mgidney <mgidney@gmail.com>
2018-12-26 15:58:51 +1100

    commit me please

--- tractor/houdinihandler.py
+++ tractor/houdinihandler.py
@@ -6,13 +6,15 @@ import platform
 class houdinihandler(TrEnvHandler):
     def __init__(self, name, envkeydict, envkeys):
         self.logger = logging.getLogger('tractor-blade')
-        self.logger.debug("initializing nukehandler: %s" % (name))
+        self.logger.debug("initializing houdinihandler: %s" % (name))
         TrEnvHandler.__init__(self, name, envkeydict, envkeys)
 
     def updateEnvironment(self, cmd, env, envkeys):
         self.logger.debug("houdinihandler.updateEnvironment: %s" % self.name)
         for key in envkeys:
-            val = key[4:]
+            # Houdini
+            # 1234567
+            val = key[7:]
             self.environmentdict['TR_ENV_HOUDINIVER'] = val
 
         return TrEnvHandler.updateEnvironment(self, cmd, env, envkeys)
@@ -21,15 +23,15 @@ class houdinihandler(TrEnvHandler):
         self.logger.debug("houdinihandler.remapCmdArgs: %s" % self.name)
         argv = TrEnvHandler.remapCmdArgs(self, cmdinfo, launchenv, thisHost)
 
-        nuke_ver = launchenv['TR_ENV_HOUDINIVER']
+        houdini_ver = launchenv['TR_ENV_HOUDINIVER']
 
         p = platform.system()
         if p == 'Linux' or p == 'Window':
-            v = nuke_ver.split('v')
+            v = houdini_ver.split('v')
             argv[0] = 'Houdini' + v[0]
 
         ## Mac OSX
         else:
-            argv[0] = 'Houdini' + nuke_ver
+            argv[0] = 'Houdini' + houdini_ver
 
         return argv
